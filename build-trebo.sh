5920f6821a16207cbede3deee5067e289c7f4ad7
itswiktoragain <propalnie15@gmail.com>
2026-09-12 01:28:05 +0200

    Correct dpkg package-list newline format

--- build-trebo.sh
+++ build-trebo.sh
@@ -61,7 +61,7 @@ apt-get install -y --no-install-recommends \
   plymouth plymouth-theme-spinner plymouth-label
 
 mapfile -t purge_candidates < <(
-  dpkg-query -W -f='${Package}\\n' 2>/dev/null | \
+  dpkg-query -W -f='${Package}\n' 2>/dev/null | \
   grep -E '^(ubuntu-desktop|ubuntu-desktop-minimal|ubuntu-minimal|ubuntu-standard|ubuntu-session|ubuntu-settings|ubuntu-wallpapers.*|ubuntu-docs|ubuntu-report|ubuntu-mono|ubuntu-touch-sounds|ubuntu-sounds|ubuntu-artwork|branding-ubuntu|fonts-ubuntu|gnome-shell-extension-ubuntu-dock)$' || true
 )
 
